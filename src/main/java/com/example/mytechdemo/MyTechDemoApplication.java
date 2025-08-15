29191b2efce93d1edcf919b82448ead6c9f45ac1
zhuang <hello@zhuang.cn>
2025-08-15 14:04:01 +0800

    测试提交

---
+++ src/main/java/com/example/mytechdemo/MyTechDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytechdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTechDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTechDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mytechdemo/MyTechDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytechdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTechDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
